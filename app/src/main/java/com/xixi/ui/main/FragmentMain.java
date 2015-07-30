458d9b1e591deca44002688c1213519864b0192f
lihaocruiser <770753119@qq.com>
2015-07-30 15:20:41 +0800

    opt fragment, opt UI

---
+++ app/src/main/java/com/xixi/ui/main/FragmentMain.java
@@ -0,0 +1,7 @@
+package com.xixi.ui.main;
+
+/**
+ * Created by 浩 on 2015-7-30.
+ */
+public class FragmentMain {
+}
---
+++ app/src/main/java/com/xixi/util/dialog/ProgressDialog.java
@@ -0,0 +1,7 @@
+package com.xixi.util.dialog;
+
+/**
+ * Created by 浩 on 2015-7-29.
+ */
+public class ProgressDialog {
+}
